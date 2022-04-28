9a0f404ec3313f99a24eeda764ea33cc37d1f831
Juyoung <godante27@gmail.com>
2022-04-28 21:20:57 +0900

    feat: Add applyToken util

---
+++ app/utils/applyToken.js
@@ -0,0 +1,14 @@
+const applyToken = () => {
+  if (headers && headers.authorization) {
+    const parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+export default applyToken;
